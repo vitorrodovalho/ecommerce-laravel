5cac8579d51b7cd794c0bb5d274f80e2a08225ad
Vitor Toledo <vitortoledorodovalho@gmail.com>
2021-06-07 00:24:12 -0300

    correcao pedidos

--- app/Http/Controllers/Voyager/OrdersController.php
+++ app/Http/Controllers/Voyager/OrdersController.php
@@ -5,41 +5,48 @@ namespace App\Http\Controllers\Voyager;
 use App\Order;
 use Illuminate\Support\Facades\DB;
 use Validator;
+use App\Product;
+use App\Category;
+use App\CategoryProduct;
 use Illuminate\Http\Request;
 use TCG\Voyager\Facades\Voyager;
+use TCG\Voyager\Events\BreadDataAdded;
+use TCG\Voyager\Events\BreadDataUpdated;
 use TCG\Voyager\Http\Controllers\VoyagerBaseController;
 
 class OrdersController extends VoyagerBaseController
 {
     public function show(Request $request, $id)
     {
+
         $slug = $this->getSlug($request);
 
         $dataType = Voyager::model('DataType')->where('slug', '=', $slug)->first();
-
+        /*
         // Compatibility with Model binding.
         $id = $id instanceof Model ? $id->{$id->getKeyName()} : $id;
 
-        /*
         //$relationships = $this->getRelationships($dataType);
         if (strlen($dataType->model_name) != 0) {
-            $model = app($dataType->model_name);
+            //$model = app($dataType->model_name);
             //$dataTypeContent = call_user_func([$model->with($relationships), 'findOrFail'], $id);
             $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
         } else {
             // If Model doest exist, get data from table name
             $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
         }*/
+
         $dataTypeContent = DB::table($dataType->name)->where('id', $id)->first();
 
+        /*
         // Replace relationships' keys for labels and create READ links if a slug is provided.
-        //$dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType, true);
+        $dataTypeContent = $this->resolveRelations($dataTypeContent, $dataType, true);
 
         // If a column has a relationship associated with it, we do not want to show that field
         $this->removeRelationshipField($dataType, 'read');
 
         // Check permission
-        //$this->authorize('read', $dataTypeContent);
+        //$this->authorize('read', $dataTypeContent);*/
 
         // Check if BREAD is Translatable
         $isModelTranslatable = is_bread_translatable($dataTypeContent);
